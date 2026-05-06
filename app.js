edd5357751320e08acb092ac88552a612b17ce0d
Celal Mete <87307659+CelalMete@users.noreply.github.com>
2026-05-06 13:24:05 +0300

    Gereksiz dosyalar nihai olarak temizlendi

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const multer = require('multer');
+require('dotenv').config();//no need for multer for now 
+const path = require('path');//same
+const fs = require('fs');//probably same
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')));//important
+app.use(express.json());
+app.set('view engine', 'ejs');
+app.set('public', path.join(__dirname, 'public'));//important2
+app.use(express.urlencoded({ extended: true }));
+//no need for database i think and i hope
+//code start point
+
+app.get('/',async(req,res)=>{
+   res.render('main',{
+      content:'home',
+      style:'store.css'
+   })
+})
+const PORT = 2000;
+app.listen(PORT, () => {
+    console.log(`Nodemon aktif: http://localhost:${PORT}/ adresine git.`);
+});
