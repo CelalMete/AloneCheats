4993ff6326953da5c963335342d62c39e5453d1a
Celal Mete <87307659+CelalMete@users.noreply.github.com>
2026-05-14 01:21:03 +0300

    swaper stage1 no photo error type shi

---
+++ public/js/swiper.js
@@ -0,0 +1,39 @@
+// Swiper Başlatma
+const swiper = new Swiper(".mySwiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  // Karşılaştırma slider'ı çekilirken ana slider kaymasın diye:
+  touchStartPreventDefault: false 
+});
+
+// Karşılaştırma Mantığı
+const inputs = document.querySelectorAll('.slider-input');
+
+inputs.forEach(input => {
+  input.addEventListener('input', (e) => {
+    const container = e.target.parentElement;
+    const foreground = container.querySelector('.foreground-img');
+    const handle = container.querySelector('.slider-handle');
+    
+    const val = e.target.value;
+    
+    foreground.style.width = `${val}%`;
+    handle.style.left = `${val}%`;
+    
+    // Slider kullanılırken Swiper'ın kaymasını engellemek için
+    if(val > 0 && val < 100) {
+        swiper.allowTouchMove = false;
+    }
+  });
+
+  // Mouse bırakıldığında Swiper tekrar aktif olsun
+  input.addEventListener('change', () => {
+    swiper.allowTouchMove = true;
+  });
+});
